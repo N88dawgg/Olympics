e9ef64a797ad70f92e19c1d911afe4f57d281db8
NathanC <n88dawgg@gmail.com>
2026-04-16 12:59:38 -0700

    Initial Commit

---
+++ Olympics/OlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympicsApp.swift
+//  Olympics
+//
+//  Created by CABRERA, NATHAN on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct OlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
